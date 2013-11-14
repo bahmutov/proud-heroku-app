51e0eabbdaa3219411d85e7b18c2ba6a6555029f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-11-13 19:58:03 -0500

    getting report for user

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-var connect = require('connect')
-  , http = require('http');
+var connect = require('connect');
+var http = require('http');
+var proud = require('proud');
+// var proudBadge = require('proud-badge');
+var report = require('./src/report');
 
-var port = env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 var app = connect()
   .use(connect.favicon())
   .use(connect.logger('dev'))
   .use(connect.static('public'))
-  .use(connect.directory('public'))
-  .use(connect.cookieParser())
-  .use(connect.session({ secret: 'my secret here' }))
-  .use(function(req, res){
-    res.end('Hello from Connect!\n');
+  .use(connect.query())
+  .use(function (req, res){
+    if (!req.url || req.url === '/') {
+      res.writeHead(401, 'missing NPM username');
+      res.end();
+      return;
+    }
+
+    var username = req.url.split('/')[1];
+    if (!username) {
+      res.writeHead(401, 'missing NPM username');
+      res.end();
+      return;
+    }
+    console.log(username);
+
+    proud(username)
+    .then(report.bind(null, username))
+    .then(function (report) {
+      console.log('report', report);
+
+      if (!report) {
+        res.end(username + ' has no modules\n');
+      } else {
+        res.end(report);
+      }
+    })
+    .catch(console.error);
   });
 
 http.createServer(app).listen(port);
